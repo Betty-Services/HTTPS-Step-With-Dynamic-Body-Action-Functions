997c07baf45714390b63f981cd42e26ecc76834a
Maarten Geerse <mgeerse@gmail.com>
2023-02-28 06:32:49 -0500

    feat: update version

---
+++ functions/http-json/1.1/index.js
@@ -0,0 +1,37 @@
+const parseHeaders = (headers) =>
+  headers.reduce((acc, { key, value }) => ({ ...acc, [key]: value }), {});
+
+const parseQueryParameters = (queryParameters) =>
+  queryParameters
+    .map(({ key, value }, index) => {
+      const paramKey = index === 0 ? `?${key}` : key;
+      return `${paramKey}=${encodeURIComponent(value)}`;
+    })
+    .join('&');
+
+const generateUrl = (url, protocol, queryParameters) =>
+  `${protocol}://${url}${parseQueryParameters(queryParameters)}`;
+
+const httpText = async ({
+  url,
+  method,
+  headers = [],
+  protocol,
+  queryParameters = [],
+  body = '',
+}) => {
+  const fetchUrl = generateUrl(url, protocol, queryParameters);
+  const options = {
+    method,
+    headers: parseHeaders(headers),
+    body: method !== 'get' && body !== '' ? body : undefined,
+  };
+
+  const response = await fetch(fetchUrl, options);
+  const json = await response.json();
+  return {
+    as: json,
+  };
+};
+
+export default httpText;
---
+++ functions/http-text/1.1/index.js
@@ -0,0 +1,37 @@
+const parseHeaders = (headers) =>
+  headers.reduce((acc, { key, value }) => ({ ...acc, [key]: value }), {});
+
+const parseQueryParameters = (queryParameters) =>
+  queryParameters
+    .map(({ key, value }, index) => {
+      const paramKey = index === 0 ? `?${key}` : key;
+      return `${paramKey}=${encodeURIComponent(value)}`;
+    })
+    .join('&');
+
+const generateUrl = (url, protocol, queryParameters) =>
+  `${protocol}://${url}${parseQueryParameters(queryParameters)}`;
+
+const httpText = async ({
+  url,
+  method,
+  headers = [],
+  protocol,
+  queryParameters = [],
+  body = null,
+}) => {
+  const fetchUrl = generateUrl(url, protocol, queryParameters);
+  const options = {
+    method,
+    headers: parseHeaders(headers),
+    body: method !== 'get' && body !== null ? body : undefined,
+  };
+
+  const response = await fetch(fetchUrl, options);
+
+  return {
+    as: await response.text(),
+  };
+};
+
+export default httpText;
